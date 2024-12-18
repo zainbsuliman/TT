96044cf51f69a63c54056720530cda0145e5cb18
zainbsuliman <zat301099@gmail.com>
2024-12-18 09:29:01 +0300

    Initial Commit

---
+++ TT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TT
+//
+//  Created by Zainab Alatwi  on 17/06/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TT/TTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TTApp.swift
+//  TT
+//
+//  Created by Zainab Alatwi  on 17/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct TTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
